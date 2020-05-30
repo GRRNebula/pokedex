afd32eacc7cf089f5258a64536601fa1d826b2d4
Pähkinä Katainen <pahkina.katainen@gmail.com>
2020-05-30 16:58:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Всем привет!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vm.tiktok.com/KPmWRN/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Suomipoikapokemon
         </a>
       </header>
     </div>
