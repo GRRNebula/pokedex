b09104279b2ead936482894ce412b9446f796ca1
Pähkinä Katainen <pahkina.katainen@gmail.com>
2020-05-30 19:48:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from href="https://assets.pokemon.com/assets/cms2/img/pokedex/full/683.png"
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+       // <img src={logo} className="App-logo" alt="logo" />
         <p>
           Всем привет!
         </p>
