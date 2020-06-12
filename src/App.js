547f9670a93f50417f40fd9585ca9ebb702785ff
Pähkinä Katainen <pahkina.katainen@gmail.com>
2020-06-12 04:14:09 +0300

    Lisätty paikalliseen tilaan sidottu syöttökenttä
    Lisätty paikallisen komponentin tilan asettava syöttökenttä ja se tila esitetty otsikossa.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import React from 'react';
 import './App.css';
 import PokedexHeader from './components/PokedexHeader'
 import Tiktokpokemon from './components/tiktokkaajapokemon'
-import { Button, Segment } from 'semantic-ui-react'
+import {Input, Segment } from 'semantic-ui-react'
 
 
 
 class App extends React.Component {
- // const pokemonpic = <img src="aromatissepokemon.png"/>
   constructor(props) {
     super(props)
     this.state = {
+      author: 'GRR',
       name: "Pähkinäinen"
     }
+    this.onChange = this.onChange.bind(this)
   }
+
+  onChange(event, data) {
+    this.setState({
+      name: data.value
+    })
+  }
+
   render() { 
+    console.log(this.state)
     return (
       <div className="App">
         <PokedexHeader 
-          author={'GRR'} 
+          author={this.state.author}
           nimi={this.state.name}/>
+          <Segment basic>
+          <Input
+            label="Kirjoita teksti tähän:"
+            onChange={this.onChange}
+            value={this.state.name}
+            />
+          </Segment>
         <Tiktokpokemon/>
       </div>
     );
